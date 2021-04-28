a23e14ed935fc58d92b237bc63b7149c7801cff7
Lucca Radaeli <radaelidev@gmail.com>
2021-04-27 23:32:26 -0300

    add exit button

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
   };
 
   const ImageIcon = () => (
-    <ImgIcon onClick={() => auth.signOut()}>
+    <ImgIcon>
       <svg
         aria-hidden="true"
         focusable="false"
